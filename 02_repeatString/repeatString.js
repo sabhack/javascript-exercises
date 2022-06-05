32ea37cd3bf2a5c15f529f45d6edfce56bacdcbe
Sabhack <mi.sabhack@gmail.com>
2022-06-05 14:28:22 +0100

    Completed the repeatString test task

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let output = "";
+    for(let i=0; i<num; i++) {
+        output += str;
+    }
+
+    return output;
 
 };
+
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
