fd3f7a47a6b39a142f135ca7684375f43bae9f11
Sabhack <mi.sabhack@gmail.com>
2022-06-05 14:54:14 +0100

    Completed the reverse funtion integration

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,22 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    
+    let splitString = str.split(""); // spliting the string into single arrays
+
+    // Using the reverse() method
+    let reverseArray = splitString.reverse();
+
+    //Using the join() method
+    let joinArray = reverseArray.join("");
+
+    //Return the reversed string
+    return joinArray;
 
 };
+
+reverseString("hello");
+reverseString("hello there");
+reverseString("123! abc!");
+reverseString("");
 
 // Do not edit below this line
 module.exports = reverseString;
