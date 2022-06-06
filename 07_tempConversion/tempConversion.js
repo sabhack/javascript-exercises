9fd1b824801d4e6090757c0fabcc7d2726a62218
Sabhack <mi.sabhack@gmail.com>
2022-06-06 12:42:03 +0100

    Completed the tasks as instructed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,27 @@
-const ftoc = function() {
+const ftoc = function(fahrenheit) {
+
+  let fTemp = fahrenheit;
+  let fToC = (fTemp - 32) * 5/9;
+  let rounded = Math.round(fToC * 10) / 10;
+  return rounded;
+};
+
+const ctof = function(celsius) {
+
+  let cTemp = celsius;
+  let cToF = cTemp * 9/5 + 32;
+  let rounded = Math.round(cToF * 10) / 10;
+  return rounded;
 
 };
 
-const ctof = function() {
+ftoc(32);
+ftoc(100);
+ftoc(-100);
 
-};
+ctof(0);
+ctof(73.2);
+ctof(-10);
 
 // Do not edit below this line
 module.exports = {
