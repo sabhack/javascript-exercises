5675ffd3fee5dcf216eb7f71c7da54c3687f6561
Sabhack <mi.sabhack@gmail.com>
2022-06-06 11:23:41 +0100

    Solved 5 out of 6 tasks

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    if (isNaN(a) || isNaN(b)) return 'ERROR';
+    if (a<0 || b<0) return 'ERROR';
 
+    return (Math.abs(a-b) + 1)*(a+b)/2;
 };
+
+sumAll(1, 4);
+sumAll(1, 4000);
+sumAll(123, 1);
+sumAll(-10, 4);
+sumAll(10, "90");
+sumAll(10, [90, 1]);
 
 // Do not edit below this line
 module.exports = sumAll;
