e7f039c9b9a8244a810d7b121d4928468669f0f7
Sabhack <mi.sabhack@gmail.com>
2022-06-06 11:58:49 +0100

    Completed the tasks 6/6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    return (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0);
 };
+
+leapYears(1996);
+leapYears(1997);
+leapYears(34992);
+leapYears(1900);
+leapYears(1600);
+leapYears(700);
 
 // Do not edit below this line
 module.exports = leapYears;
