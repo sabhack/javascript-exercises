a27af4f5ccb88909e67117c07fecfb26ea1358ca
Sabhack <mi.sabhack@gmail.com>
2022-06-06 08:24:42 +0100

    Solved 3 out of 7 tests in this exercise of removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...value) {
+    
+    return arr.filter(x => !value.includes(x))
 };
-
+removeFromArray([1,2,3,4], 3);
+removeFromArray([1, 2, 3, 4], 3, 2);
+removeFromArray([1, 2, 3, 4], 7, "tacos");
 // Do not edit below this line
 module.exports = removeFromArray;
