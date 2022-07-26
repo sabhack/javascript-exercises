6f938a8f7a07d7fbe601c235466260b4ea62c544
Sabhack <mi.muhammad@yahoo.com>
2022-07-26 10:13:38 +0100

    completed the function of fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,30 @@
-const fibonacci = function() {
+// const fibonacci = function fib(num) {
+//     if(num == 0) {
+//         return 0;
+//     } else if (num == 1) {
+//         return 1;
+//     } else {
+//         return fib(num - 1) + fib(num - 2);
+//     }
 
-};
+// };
+
+const fibonacci = n => {
+    if (n <= 1) {
+        return n;
+    }
+    return fibonacci(n-1) + fibonacci(n-2)
+}
+
+fibonacci(4)
+fibonacci(6)
+fibonacci(10)
+fibonacci(15)
+fibonacci(25)
+fibonacci(-25)
+fibonacci("1")
+fibonacci("2")
+fibonacci("8")
 
 // Do not edit below this line
 module.exports = fibonacci;
