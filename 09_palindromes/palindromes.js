a1f4f10734a27f7feb58371e66d2352f49a4128c
Sabhack <mi.muhammad@yahoo.com>
2022-07-26 09:32:51 +0100

    Completed function to dertermine a str palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function palindrome (str) {
+    let re = /[\W_]/g;
 
+    let lowRegStr = str.toLowerCase().replace(re, '');
+
+    let reverseStr = lowRegStr.split('').reverse().join('');
+
+    return reverseStr === lowRegStr;
 };
+
+palindromes('racecar')
+palindromes('racecar!')
+palindromes('Racecar!')
+palindromes('A car, a man, a maraca.')
+palindromes('Animal loots foliated detail of stool lamina.')
+palindromes('ZZZZ car, a man, a maracaz.')
 
 // Do not edit below this line
 module.exports = palindromes;
